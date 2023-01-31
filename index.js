05f6b3e61095e7a916ee992b2a3f94a8ce984d49
MichaelBinnick <mbinnick@live.ca>
2023-01-31 09:16:33 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+// backend exposing API
+
+// adhere to REST standards
+
+// TASK 1
+
+// social media app
+
+// SECTION 1
+
+// user reg unique username and password
+// user login (any means I'm comfortable with)
+// unit tests for these basic methods
+
+// make error free, and don't move on until complete
+
+
+// SECTION 2
+
+// Create, read, update, delete, tweets
+// Unit/integration tests for ALL endpoints I've built so far
+// (basic and extended functionality)
+
+
+// OPTIONAL SECTION 3
+
+// like/unlike a tweet
+// retweet
+// threading
+
+
+// TASK 2
+
+// Build a stock market tracking system where user’s can trade stocks or shares.
+
+// Your system should have support for users to login/logout.
+
+// Users should be able to add balance to their wallet.
+
+// Users should be able to buy/sell shares (transactions need not be stored)
+
+// Users should be able to subscribe to an endpoint that should provide live rates.
+
+// Users should have the ability to see their portfolio
